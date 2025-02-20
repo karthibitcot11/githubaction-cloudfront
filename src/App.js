3d99f22df3deaf403390c8ca129e03d8b9562429
karthibitcot11 <karthikeyanm01196@gmail.com>
2025-02-20 12:05:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function Main() {
   // eslint-disable-next-line
   const [currentTheme, setCurrentTheme] = useState(initialTheme);
 
-  console.log('this is a test');
+  console.log('this is a test);
   
   return (
     <ChakraProvider theme={currentTheme}>
